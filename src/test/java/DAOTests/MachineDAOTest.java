07768b99ee71317b852302a26fa1fb7bc47ded4f
Milan Valusek <xvalusek@nymfe66.fi.muni.cz>
2014-10-07 16:35:50 +0200

    added main entities - jsut classes for first download

---
+++ src/test/java/DAOTests/MachineDAOTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAOTests;
+
+/**
+ *
+ * @author xvalusek
+ */
+public class MachineDAOTest {
+    
+}
