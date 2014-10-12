ed022ba9611621cacb041a6e84a8e384b9b385f6
milos <milos@myubuntu>
2014-10-12 22:46:59 +0200

    Test for Rent update 3

--- src/test/java/Entity/DaoImpl/RentDaoImplTest.java
+++ src/test/java/Entity/DaoImpl/RentDaoImplTest.java
@@ -18,10 +18,19 @@ import javax.persistence.Persistence;
 import javax.persistence.PersistenceUnit;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -38,11 +47,12 @@ public class RentDaoImplTest {
     private RentDaoImpl instance;
     private Machine machine1;
     private Customer customer1;
+    private Customer customer2;
     private Rent rent1;
 
     
     public RentDaoImplTest() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("rent");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("stroje");
         em = emf.createEntityManager();
         instance = new RentDaoImpl(em);
 
@@ -74,17 +84,24 @@ public class RentDaoImplTest {
 
         
         Customer customer1 = new Customer();
-
+        Customer customer2 = new Customer();
         customer1.setFirstName("Jan");
         customer1.setSecondName("Zhanal");
         customer1.setAddress("tehnicka");
         customer1.setLegalStatus(LegalStatus.NATURAL);
 
+        customer2.setFirstName("Ion");
+        customer2.setSecondName("Fustatsu");
+        customer2.setAddress("Ceska");
+        customer2.setLegalStatus(LegalStatus.LEGAL); 
         
         em.persist(customer1);
         em.refresh(customer1);
         this.customer1 = customer1;
-     
+        em.persist(customer2);
+        em.refresh(customer2);
+        this.customer1 = customer2;
+        
     
         
         Rent rent1 = new Rent();
@@ -121,4 +138,26 @@ public class RentDaoImplTest {
         assertTrue(rent.getId() > 0);
     }    
 
+     @Test
+     public void testUpdate() {
+     em.getTransaction().begin();
+     Customer rentBefore = em.find(Rent.class, rent1.getId()).getCustomer();
+             rent1.setCustomer(customer2);
+     instance.update(rent1);
+     Customer rentNew = em.find(Rent.class, rent1.getId()).getCustomer();
+     em.getTransaction().commit();
+     
+         assertNotSame(rentBefore,rentNew);
+     }    
+     
+     @Test
+     public void testRemove() {
+     em.getTransaction().begin();
+     Rent rentBefore = em.find(Rent.class, rent1.getId());
+         assertNotNull(rentBefore);
+     instance.remove(rent1);
+     Rent rentNew = em.find(Rent.class, rent1.getId());
+     assertNull(rentNew);
+     em.getTransaction().commit();
+     }     
 }
