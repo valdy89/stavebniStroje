7d5ee424079acdd0a13714e3fd61552fbe821dd7
milos <milos@myubuntu>
2014-10-12 23:34:28 +0200

    comment added

--- src/main/java/Entity/Dao/CustomerDao.java
+++ src/main/java/Entity/Dao/CustomerDao.java
@@ -9,19 +9,47 @@ import Entity.Customer;
 import java.util.List;
 
 /**
+ * This interface is used for adding, updating,removing and searching customers
+ * in the DB
  *
  * @author milos
  */
 public interface CustomerDao {
 
+    /**
+     * Method for adding customer to the DB
+     *
+     * @param customer customer which should be added to the DB
+     */
     public void persist(Customer customer);
 
+    /**
+     * This method returns customer which is owner of the Id
+     *
+     * @param id id of the customer which we want to find
+     * @return customer which is owner of the given id
+     */
     public Customer findById(Long id);
 
+    /**
+     * method is used for updating customer in the DB
+     *
+     * @param customer which needs to be updated
+     */
     public void update(Customer customer);
 
+    /**
+     * This method is used to remove customer from the DB
+     *
+     * @param customer which should be removed from the DB
+     */
     public void remove(Customer customer);
 
+    /**
+     * This method returns all customers from the DB
+     *
+     * @return list of customers from the DB
+     */
     public List<Customer> findAll();
 
 }
