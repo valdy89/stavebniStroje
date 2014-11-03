d5f45b3cf55a4b030a5388cd7b1c054b522a5cc4
test <test@localhost>
2014-11-03 22:30:15 +0100

    Customer Dto & CustomerService interface

---
+++ stavebniStroje-service/src/main/java/cz/muni/stavebniStroje/service/CustomerService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.stavebniStroje.service;
+
+import cz.muni.fi.stavebniStroje.dto.CustomerDto;
+import cz.muni.fi.stavebniStroje.util.LegalStatus;
+import java.util.List;
+
+/**
+ * This class represent interface for Customer Service which has methods for work with the customer
+ *
+ * @author Milos Petrovic
+ */
+public interface CustomerService {
+    
+    public void createCustomer(CustomerDto customer);
+
+    public CustomerDto getCustomer(Long id);
+
+    public void updateCustomer(CustomerDto customer);
+
+    public void removeCustomer(CustomerDto customer);
+
+    public List<CustomerDto> findAllCustomer();
+
+}
