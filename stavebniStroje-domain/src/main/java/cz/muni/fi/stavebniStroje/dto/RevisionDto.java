4b5f45c648497fb3d79066bac8d9afc8d4e01f08
dominikdavid-cz <dominik.d@centrum.cz>
2014-11-04 15:30:27 +0100

    Insert RentDto and RevisionDto

---
+++ stavebniStroje-domain/src/main/java/cz/muni/fi/stavebniStroje/dto/RevisionDto.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.stavebniStroje.dto;
+
+import cz.muni.fi.stavebniStroje.entity.Machine;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author Dominik David
+ * 
+ * RevisionDto
+ * 
+ */
+public class RevisionDto implements Serializable {
+   
+    private long id;
+    private Machine machine;
+    private Date dateOfRevision;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Machine getMachine() {
+        return machine;
+    }
+
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
+
+    public Date getDateOfRevision() {
+        return dateOfRevision;
+    }
+
+    public void setDateOfRevision(Date dateOfRevision) {
+        this.dateOfRevision = dateOfRevision;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 83 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 83 * hash + Objects.hashCode(this.machine);
+        hash = 83 * hash + Objects.hashCode(this.dateOfRevision);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RevisionDto other = (RevisionDto) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.machine, other.machine)) {
+            return false;
+        }
+        if (!Objects.equals(this.dateOfRevision, other.dateOfRevision)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
