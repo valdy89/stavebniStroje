6a9eeb381c4fd831fc791661458d087d0537174a
test <test@localhost>
2014-11-05 20:37:27 +0100

    CustomerDaoImpl alpha 1

--- stavebniStroje-service/src/main/java/cz/muni/fi/stavebniStroje/serviceImpl/CustomerServiceImpl.java
+++ stavebniStroje-service/src/main/java/cz/muni/fi/stavebniStroje/serviceImpl/CustomerServiceImpl.java
@@ -9,12 +9,14 @@ import cz.muni.fi.stavebniStroje.dao.CustomerDao;
 import cz.muni.fi.stavebniStroje.dto.CustomerDto;
 import cz.muni.fi.stavebniStroje.entity.Customer;
 import cz.muni.stavebniStroje.service.CustomerService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -26,11 +28,13 @@ public class CustomerServiceImpl implements CustomerService {
     private EntityManager entityManager;
     CustomerDao customerDao;
 
-   //task from C2: You can use Dozer framework to map entity instances to transfer objects. The mapping may be done on Service Layer
-   // Basic: DestinationObject destObject = mapper.map(sourceObject, DestinationObject.class);
-   //source: http://dozer.sourceforge.net/documentation/usage.html    
+    @Transactional    
     @Override
     public void createCustomer(CustomerDto customerDto) {
+        if (customerDto == null) {
+            throw new NullPointerException("Argument customerDto was null");
+        }
+        
         Customer customer = mapper.map(customerDto, Customer.class);
 
         entityManager.getTransaction();
@@ -38,22 +42,53 @@ public class CustomerServiceImpl implements CustomerService {
         entityManager.close();
     }
 
-    @Override
-    public CustomerDto getCustomer(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @Required
+    public void setCustomerDao(CustomerDao customerDao) {
+        this.customerDao = customerDao;
     }
 
-    @Override
-    public void updateCustomer(CustomerDto customer) {
-    }
-
-    @Override
-    public void removeCustomer(CustomerDto customer) {
-    }
-
+    @Transactional 
     @Override
     public List<CustomerDto> findAllCustomer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<CustomerDto> customers = new ArrayList<>();
+        for (Customer customer : customerDao.findAll()) {
+            customers.add(mapper.map(customer, CustomerDto.class));
+        }
+        return customers;
+    }
+
+    @Transactional
+    @Override
+    public void updateCustomer(CustomerDto customerDto) {
+        if (customerDto == null) {
+            throw new NullPointerException("Argument customerDto was null");
+        }
+        Customer customer = mapper.map(customerDto, Customer.class);
+        customerDao.update(customer);
+
+    }
+
+    @Transactional
+    @Override
+    public void removeCustomer(CustomerDto customerDto) {
+        if (customerDto == null) {
+            throw new NullPointerException("Argument customerDto was null");
+        }
+        Customer customer = mapper.map(customerDto, Customer.class);
+        customerDao.remove(customer);
+
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public CustomerDto getCustomer(Long id) {
+        if (id == null) {
+            throw new NullPointerException("Argument id was null");
+        }
+        Customer customer;
+        customer = customerDao.findById(id);
+        return mapper.map(customer, CustomerDto.class);
+
     }
 
     @Required
