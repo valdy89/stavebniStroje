aad5b278f7337691b2af06b5b5d1f0d14f785e73
spito <jiri.weiser@gmail.com>
2014-12-04 13:06:18 +0100

    web: Fix MachineActionBean.

--- stavebniStroje-web/src/main/java/cz/muni/fi/stavebnistroje/web/MachineActionBean.java
+++ stavebniStroje-web/src/main/java/cz/muni/fi/stavebnistroje/web/MachineActionBean.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cz.muni.fi.stavebnistroje.web;
 
 import cz.muni.fi.stavebniStroje.dto.MachineDto;
 import cz.muni.fi.stavebniStroje.service.MachineService;
+import java.util.Collection;
 import java.util.List;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.Before;
@@ -32,49 +32,46 @@ import org.springframework.dao.InvalidDataAccessApiUsageException;
  * @author milos
  */
 @UrlBinding("/machine/{$event}/")
-public class MachineActionBean  extends BaseActionBean  {
+public class MachineActionBean extends BaseActionBean {
     
     private ActionBeanContext context;
     final static Logger log = LoggerFactory.getLogger(MachineActionBean.class);
     @SpringBean
     protected MachineService machineService;    
     
-    
     @ValidateNestedProperties({
         @Validate(on = {"add", "update", "save"}, field = "name", required = true),
         @Validate(on = {"add", "update", "save"}, field = "type", required = true),
         @Validate(on = {"add", "update", "save"}, field = "description", required = true),
-        @Validate(on = {"add", "update", "save"}, field = "price", required = true),
-    })
+        @Validate(on = {"add", "update", "save"}, field = "price", required = true),})
     private MachineDto machine;
-    private List<MachineDto> result;
-    @Autowired
-   
+    private Collection<MachineDto> result;
     
-    public List<MachineDto> getResult() {
+    @Autowired
+    
+    public Collection<MachineDto> getResult() {
         return result;
     }
-
-    public void setResult(List<MachineDto> result) {
+    
+    public void setResult(Collection<MachineDto> result) {
         this.result = result;
     }
-
+    
     public MachineService getMachineService() {
         return machineService;
     }
-
+    
     public void setMachineService(MachineService machineService) {
         this.machineService = machineService;
     }
-
+    
     public MachineDto getMachine() {
         return machine;
     }
-
+    
     public void setMachine(MachineDto machine) {
         this.machine = machine;
     }
-    
     
     @Before(stages = LifecycleStage.BindingAndValidation, on = {"update", "save", "delete"})
     public void loadMachineFromDB() {
@@ -84,30 +81,30 @@ public class MachineActionBean  extends BaseActionBean  {
         } else {
         }
     }    
-        
+    
     public Resolution add() {
         log.debug("add() machine={}", machine);
-/*        try {
+        try {
             machineService.newMachine(machine);
         } catch (InvalidDataAccessApiUsageException e) {
             return new ForwardResolution("/fail/Fail.jsp");
-        }*/
+        }
         result = (List<MachineDto>) machineService.findAllMachines();
-        return new RedirectResolution(this.getClass(),"list");
+        return new RedirectResolution(this.getClass(), "list");
     }
-
+    
     public Resolution edit() {
         log.debug("update() machine={}", machine);
         machineService.updateMachine(machine);
-        return new ForwardResolution("/machine/edit.jsp");
+        return new ForwardResolution(this.getClass(), "edit");
     }
-
+    
     public Resolution save() {
         log.debug("save() machine={}", machine);
         machineService.updateMachine(machine);
         return new ForwardResolution(this.getClass(), "list");
     }
-
+    
     public Resolution delete() {
         log.debug("delete({})", context.getRequest().getParameter("machine.id"));
         String id = context.getRequest().getParameter("machine.id");
@@ -117,13 +114,13 @@ public class MachineActionBean  extends BaseActionBean  {
             return new RedirectResolution("/fail/Fail.jsp");
         }
         return new RedirectResolution(this.getClass(), "list");
-    }        
-
+    }    
+    
     @DefaultHandler
     public Resolution list() {
         log.debug("list()");
-        result = (List<MachineDto>) machineService.findAllMachines();
+        result = machineService.findAllMachines();
         return new ForwardResolution("/machine/list.jsp");
     }
-
+    
 }
