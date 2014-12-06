69d18a3ef5e3b90911564daf1868556f70bb6f4d
spito <jiri.weiser@gmail.com>
2014-12-06 21:42:57 +0100

    implementations: Remove useles imports.

--- stavebniStroje-implementations/src/main/java/cz/muni/fi/stavebniStroje/serviceImpl/RentServiceImpl.java
+++ stavebniStroje-implementations/src/main/java/cz/muni/fi/stavebniStroje/serviceImpl/RentServiceImpl.java
@@ -20,8 +20,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import org.dozer.DozerBeanMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.dao.DataAccessException;
