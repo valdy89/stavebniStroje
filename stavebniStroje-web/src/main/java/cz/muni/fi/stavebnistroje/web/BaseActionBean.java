f8aebb5fa2bc972528dff2d4246a41f14be9c8ba
spito <jiri.weiser@gmail.com>
2014-12-06 16:59:54 +0100

    web: Add redirect method into BaseActionBean.

--- stavebniStroje-web/src/main/java/cz/muni/fi/stavebnistroje/web/BaseActionBean.java
+++ stavebniStroje-web/src/main/java/cz/muni/fi/stavebnistroje/web/BaseActionBean.java
@@ -7,6 +7,9 @@ package cz.muni.fi.stavebnistroje.web;
 
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.RedirectResolution;
+import net.sourceforge.stripes.action.Resolution;
 import org.apache.taglibs.standard.functions.Functions;
 
 /**
@@ -16,6 +19,18 @@ import org.apache.taglibs.standard.functions.Functions;
 public abstract class BaseActionBean implements ActionBean {
 
     private ActionBeanContext context;
+
+    private String continueTo;
+    final private String defaultContinueTo;
+
+    public BaseActionBean(String defaultContinueTo) {
+        this.defaultContinueTo = defaultContinueTo;
+    }
+    public BaseActionBean() {
+        this.defaultContinueTo = "";
+    }
+    
+    
 
     @Override
     public void setContext(ActionBeanContext context) {
@@ -29,6 +44,29 @@ public abstract class BaseActionBean implements ActionBean {
 
     public static String escapeHTML(String s) {
         return Functions.escapeXml(s);
+    }
+
+    public String getContinueTo() {
+        return continueTo;
+    }
+
+    public void setContinueTo(String continueTo) {
+        this.continueTo = continueTo;
+    }
+
+    protected Resolution redirect() {
+        return redirect("");
+    }
+    
+    protected Resolution redirect(String path) {
+        if (path.isEmpty()) {
+            path = defaultContinueTo;
+        }
+        if (continueTo != null && !continueTo.isEmpty()) {
+            path = continueTo;
+        }
+
+        return new RedirectResolution(path);
     }
 
 }
