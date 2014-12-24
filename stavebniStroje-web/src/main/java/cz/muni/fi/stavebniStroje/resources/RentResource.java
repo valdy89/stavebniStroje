98620fbf88f15f289b9ad942312124f81d57c0af
spito <jiri.weiser@gmail.com>
2014-12-24 15:40:53 +0100

    Add Rent controller and resource.

---
+++ stavebniStroje-web/src/main/java/cz/muni/fi/stavebniStroje/resources/RentResource.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.stavebniStroje.resources;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import cz.muni.fi.stavebniStroje.dto.CustomerDto;
+import cz.muni.fi.stavebniStroje.dto.MachineDto;
+import cz.muni.fi.stavebniStroje.dto.RentDto;
+import java.util.Date;
+
+/**
+ *
+ * @author spito
+ */
+public class RentResource {
+    
+    private Long id;
+    private Long machineId;
+    private Long customerId;
+    
+    private Date from;
+    private Date to;
+    
+    @JsonCreator
+    public RentResource(
+            @JsonProperty("machineId") Long machineId,
+            @JsonProperty("customerId") Long customerId,
+            @JsonProperty("from") Date from,
+            @JsonProperty("to") Date to) {
+        setId(new Long(0));
+        setMachineId(machineId);
+        setCustomerId(customerId);
+        setFrom(from);
+        setTo(to);
+    }
+    
+    public RentResource(RentDto rentDto) {
+        setId(rentDto.getId());
+        setMachineId(rentDto.getMachine().getId());
+        setCustomerId(rentDto.getCustomer().getId());
+        setFrom(rentDto.getStartOfRent());
+        setTo(rentDto.getEndOfRent());
+    }
+    
+    public RentDto toDto() {
+        RentDto r = new RentDto();
+        r.setId(getId());
+        r.setMachine(new MachineDto());
+        r.getMachine().setId(getMachineId());
+        r.setCustomer(new CustomerDto());
+        r.getCustomer().setId(getCustomerId());
+        r.setStartOfRent(getFrom());
+        r.setEndOfRent(getTo());
+        return r;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(Long machineId) {
+        this.machineId = machineId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+    
+}
---
+++ stavebniStroje-web/src/main/java/cz/muni/fi/stavebniStroje/rest/controllers/RentsController.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.stavebniStroje.rest.controllers;
+
+import cz.muni.fi.stavebniStroje.dto.RentDto;
+import cz.muni.fi.stavebniStroje.resources.RentResource;
+import cz.muni.fi.stavebniStroje.service.RentService;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author spito
+ */
+@RestController
+@RequestMapping("/service/rent")
+public class RentsController {
+
+    final static Logger log = LoggerFactory.getLogger(RentsController.class);
+
+    @Autowired
+    private RentService rentService;
+
+    public void setRentService(RentService rentService) {
+        this.rentService = rentService;
+    }
+    
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    RentResource getRent(@PathVariable("id") Long id) {
+        RentDto rentDto = rentService.findRentById(id);
+        return new RentResource(rentDto);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+    public @ResponseBody
+    RentResource createRent(@RequestBody RentResource rentResource) {
+        rentService.newRent(rentResource.toDto());
+        return rentResource;
+    }
+
+    public @ResponseBody
+    Collection<RentResource> listAll() {
+        Collection<RentDto> rents = rentService.findAllRent();
+        Collection<RentResource> rentResources = new ArrayList<>();
+        
+        for (RentDto r : rents) {
+            rentResources.add(new RentResource(r));
+        }
+        return rentResources;
+    }
+    
+    @RequestMapping(value = "date/{date}", method = RequestMethod.GET)
+    public @ResponseBody
+    Collection<RentResource> listByDate(@PathVariable("date") Date date) {
+        Collection<RentDto> rents = rentService.findRentByDate(date);
+        Collection<RentResource> rentResources = new ArrayList<>();
+        
+        for (RentDto r : rents) {
+            rentResources.add(new RentResource(r));
+        }
+        return rentResources;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT, consumes = "application/json")
+    public @ResponseBody
+    RentResource updateMachine(@PathVariable("id") Long id, @RequestBody RentResource rentResource) {
+        rentResource.setId(id);
+        rentService.updateRent(rentResource.toDto());
+        return rentResource;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public @ResponseBody
+    RentResource deleteMachine(@PathVariable("id") Long id) {
+        RentDto rentDto = rentService.findRentById(id);
+        RentResource machineResource = new RentResource(rentDto);
+        rentService.removeRent(rentDto);
+        return machineResource;
+    }
+
+}
